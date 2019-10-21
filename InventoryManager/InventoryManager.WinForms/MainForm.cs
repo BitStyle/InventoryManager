5a6ec664afe5413f163be7f0d660990b08ab0c35
BitStyle <hwglaspie51@gmail.com>
2019-10-21 14:16:29 -0400

    Creating project

--- InventoryManager/InventoryManager.WinForms/MainForm.cs
+++ InventoryManager/InventoryManager.WinForms/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace InventoryManager.WinForms
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
