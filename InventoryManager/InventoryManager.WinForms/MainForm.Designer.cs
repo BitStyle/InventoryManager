4b1cc4e23814fbd25d110f79633a0689b9724271
BitStyle <hwglaspie51@gmail.com>
2019-10-28 09:59:19 -0400

    Takes in filenames. Converts json to object.

--- InventoryManager/InventoryManager.WinForms/MainForm.Designer.cs
+++ InventoryManager/InventoryManager.WinForms/MainForm.Designer.cs
@@ -28,22 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Location = new System.Drawing.Point(12, 27);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(26, 13);
+            this.fileLabel.TabIndex = 0;
+            this.fileLabel.Text = "File:";
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(655, 22);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(38, 23);
+            this.selectFileButton.TabIndex = 1;
+            this.selectFileButton.Text = "...";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(35, 24);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.ReadOnly = true;
+            this.fileTextBox.Size = new System.Drawing.Size(614, 20);
+            this.fileTextBox.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "World Files (*json)|*.json";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(705, 445);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(this.fileLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Inventory Manager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
     }
 }
 
--- InventoryManager/InventoryManager.WinForms/MainForm.cs
+++ InventoryManager/InventoryManager.WinForms/MainForm.cs
@@ -7,16 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using IntentoryManager.Data;
+using System.IO;
 
 namespace InventoryManager.WinForms
 {
     public partial class MainForm : Form
     {
+        private World World { get => mWorld; set => mWorld = value; }
+
         public MainForm()
         {
             InitializeComponent();
-
-            InventoryManager.Data.
         }
+
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                World = JsonConvert.DeserializeObject<World>(File.ReadAllText(openFileDialog.FileName));
+                fileTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private World mWorld;
     }
 }
